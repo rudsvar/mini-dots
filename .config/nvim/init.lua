29077c84ecdf746a9bfacae63f28f4d0032c1d57
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-01-02 09:53:39 +0100

    Retab

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,36 @@
-require('default.packer')
 require('default.set')
 require('default.remap')
 
--- Auto save + load folds
-vim.cmd([[
-	function! MakeViewCheck()
-		if has('quickfix') && &buftype =~ 'nofile'
-			" Buffer is marked as not a file
-			return 0
-		endif
-		if empty(glob(expand('%:p')))
-			" File does not exist on disk
-			return 0
-		endif
-		return 1
-	endfunction
-	augroup vimrcAutoView
-		autocmd!
-		" Autosave & Load Views.
-		autocmd BufWritePost,BufLeave,BufWinLeave,WinLeave ?* if MakeViewCheck() | mkview! | endif
-		autocmd BufWinEnter ?* if MakeViewCheck() | normal! zx
-		autocmd BufWinEnter,BufWritePost ?* if MakeViewCheck() | silent! loadview | endif
-	augroup end
-]])
+if vim.g.vscode == nil then
+    require('default.packer')
 
-vim.cmd([[
-    augroup AutoFormat
-        au!
-        au BufWritePre * :lua vim.lsp.buf.format()
-    augroup END
-]])
+    -- Auto save + load folds
+    vim.cmd([[
+        function! MakeViewCheck()
+            if has('quickfix') && &buftype =~ 'nofile'
+                " Buffer is marked as not a file
+                return 0
+            endif
+            if empty(glob(expand('%:p')))
+                " File does not exist on disk
+                return 0
+            endif
+            return 1
+        endfunction
+        augroup vimrcAutoView
+            autocmd!
+            " Autosave & Load Views.
+            autocmd BufWritePost,BufLeave,BufWinLeave,WinLeave ?* if MakeViewCheck() | mkview! | endif
+            autocmd BufWinEnter ?* if MakeViewCheck() | normal! zx
+            autocmd BufWinEnter,BufWritePost ?* if MakeViewCheck() | silent! loadview | endif
+        augroup end
+    ]])
+
+    vim.cmd([[
+        augroup AutoFormat
+            au!
+            au BufWritePre * :lua vim.lsp.buf.format()
+        augroup END
+    ]])
+
+end
