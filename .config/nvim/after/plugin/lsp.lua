6771245748ebdac71f3971be22b2df3f393cc5af
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-01-02 09:53:39 +0100

    Change completion mapping to tab, and change some colors

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -27,12 +27,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<Tab>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
--- cmp_mappings['<CR>'] = nil
+cmp_mappings['<CR>'] = nil
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
