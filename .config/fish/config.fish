c824216d7cc6295772752f8c0957913cdad5df15
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-01-02 09:53:38 +0100

    Add config files for bash, fish,tmux and starship

---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+alias conf="git --git-dir=$HOME/git/mini-dots.git --work-tree=$HOME"
+
+starship init fish | source
---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,3 @@
+# Defined in /home/rudi/.config/fish/functions/fish_greeting.fish @ line 1
+function fish_greeting
+end
