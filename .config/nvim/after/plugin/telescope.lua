d619d98c3246fdac303f4085e5af6a7ef3b349f2
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-01-11 10:56:13 +0100

    Make telescope fall back to find_files if not in git repository

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,25 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+
+-- Fall back to find_files if git_files can't find a .git directory
+local is_inside_work_tree = {}
+
+function project_files()
+  local opts = {}
+
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+
+  if is_inside_work_tree[cwd] then
+    builtin.git_files(opts)
+  else
+    builtin.find_files(opts)
+  end
+end
+
+vim.keymap.set('n', '<C-p>', project_files, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
